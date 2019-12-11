7f04ab87d238f7a85f0b741748f0a254f8012dd2
Yikeshan Xiaokaiti <yxxiao65@iMac705-462.local>
2019-12-11 13:35:54 -0500

    5

---
+++ modules/cap6.lua
@@ -0,0 +1,11 @@
+local Map = require 'core/map'
+local Entity = require 'core/entity'
+local gamera = require 'core/gamera'
+local Util = require 'core/util'
+local anim8 = require 'core/anim8'
+
+function love.load()
+
+
+
+end
